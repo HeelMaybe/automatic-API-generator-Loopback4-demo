31ab7f5ba8da1a07a398ca1e887820ce607c2b64
HeelMaybe <ghelton1@uncc.edu>
2022-04-09 18:42:55 -0400

    first commit

---
+++ src/models/book.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Book extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  category: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  details: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  author: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  condition: string;
+
+
+  constructor(data?: Partial<Book>) {
+    super(data);
+  }
+}
+
+export interface BookRelations {
+  // describe navigational properties here
+}
+
+export type BookWithRelations = Book & BookRelations;
